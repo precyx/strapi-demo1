9e2fcd2f9920c181afa1594cd791629fe4a90942
Pascal Bachmann <pr.pascal.bachmann@gmail.com>
2025-01-01 12:28:01 +0100

    add cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,16 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:5173/'],  // SvelteKit dev URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
