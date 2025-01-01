9a791557c4718859c7080e747d45d1750abc73ee
Pascal Bachmann <pr.pascal.bachmann@gmail.com>
2025-01-01 11:19:07 +0100

    test

--- config/database.ts
+++ config/database.ts
@@ -23,7 +23,7 @@ export default ({ env }) => {
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
     postgres: {
-      connection: {
+      connection: { /**/ 
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
