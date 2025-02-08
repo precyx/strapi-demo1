57020fdaf460829864a8d6d285b3ee7230537fe9
Pascal <pr.pascal.bachmann@gmail.com>
2025-02-08 18:06:12 +0100

    fix config for railway

--- config/database.ts
+++ config/database.ts
@@ -6,36 +6,14 @@ export default ({ env }) => {
   const isProduction = env("NODE_ENV") === "production";
 
   const connections = {
-    mysql: {
-      connection: {
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
     postgres: {
       connection: isProduction
         ? {
             // Production Database (Railway)
-            connection: env("DATABASE_URL", ""),
-            ssl: env.bool("DATABASE_SSL", true),
+            connectionString: env("DATABASE_URL", ""),
+            ssl: {
+              rejectUnauthorized: false, // Required for Railway
+            },
           }
         : {
             // Local Database (PostgreSQL or SQLite)
